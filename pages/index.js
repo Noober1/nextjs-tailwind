ac444e3a8d827dc6416c035c16da39c22fa27c65
Noober1 <cucu.ruhiyatna3@gmail.com>
2021-07-28 13:52:59 +0700

    first commit

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ const Index = () => {
             <Head>
                 <title>Next.js with Tailwind CSS</title>
             </Head>
-            <div class="w-full pt-20 px-4">
+            <div className="w-full pt-20 px-4">
                 <div className="text-center">
                     <h1 className="font-extrabold font-sans text-center text-6xl mb-5">
                         Next.js with Tailwind CSS
